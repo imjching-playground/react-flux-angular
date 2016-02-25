a1af80b388aac7557d0e4ce7cb74ebe3b050679f
Jay <imjching@hotmail.com>
2016-02-25 23:52:43 +0800

    add fake eventemitters (an array of functions)

--- React/lib/app.js
+++ React/lib/app.js
@@ -1,1 +1,13 @@
 ReactDOM.render(React.createElement(Forum, null), document.getElementById("forum"));
+
+var myemitter = new EventEmitter();
+
+myemitter.on('STARTED_THE_APP', function () {
+  console.log('started the app');
+});
+
+myemitter.on('STARTED_THE_APP', function () {
+  console.log('started the app #2');
+});
+
+myemitter.emit('STARTED_THE_APP');
